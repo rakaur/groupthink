0eff094c3b2d9fdd8190f2ed5a9043cf707b9460
Eric Will <rakaur@malkier.net>
2022-05-29 12:00:00 -0500

    Style users/password/new
    * Style the password reset page
    * Configure Action Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,19 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_prefix = "streams_production"
+
+  config.action_mailer.perform_caching = false
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+
+  # For Devise
+  config.action_mailer.default_url_options = { host: ENV["MAILER_DEFAULT_URL"], protocol: "https" }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
