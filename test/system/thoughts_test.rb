2a1304e2e15219482382252c3137d45c8d08e315
Eric Will <rakaur@malkier.net>
2022-05-30 18:35:30 -0500

    Create scaffold for Thoughts
    * Create scaffolding for Thoughts
    * Change Streams#content to JSONB which is apparently a thing
    * Move faker gem into test/dev cuz is fun
    * Create proper fixtures for Streams/Thoughts/Users

---
+++ test/system/thoughts_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class ThoughtsTest < ApplicationSystemTestCase
+  setup do
+    @thought = thoughts(:one)
+  end
+
+  test "visiting the index" do
+    visit thoughts_url
+    assert_selector "h1", text: "Thoughts"
+  end
+
+  test "should create thought" do
+    visit thoughts_url
+    click_on "New thought"
+
+    fill_in "Content", with: @thought.content
+    fill_in "User", with: @thought.user_id
+    click_on "Create Thought"
+
+    assert_text "Thought was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Thought" do
+    visit thought_url(@thought)
+    click_on "Edit this thought", match: :first
+
+    fill_in "Content", with: @thought.content
+    fill_in "User", with: @thought.user_id
+    click_on "Update Thought"
+
+    assert_text "Thought was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Thought" do
+    visit thought_url(@thought)
+    click_on "Destroy this thought", match: :first
+
+    assert_text "Thought was successfully destroyed"
+  end
+end
