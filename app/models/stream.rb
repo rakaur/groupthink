79f85ec13e0b3165597c6788e936de4e46aef5e5
Eric Will <rakaur@malkier.net>
2022-05-30 23:04:41 -0500

    Work on data relations
    * Streams select Thoughts based on dynamic criteria but they essentially share as HABTM relationship which I've emulated here on the Stream#thoughts method. There's not an easy way to do Thought#streams because Streams select Thoughts but Thoughts don't know what Streams they're part of at any given moment.
    * Users HABTM Streams
    * Basic progress on views for Thoughts and Streams

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,2 +1,8 @@
 class Stream < ApplicationRecord
+  has_and_belongs_to_many :users
+
+  def thoughts
+    return Thought.where(created_at: 1.month.ago .. Time.now) if content['all']
+    Thought.where(content)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_30_222139) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_31_010419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_222139) do
     t.jsonb "content", default: "{}", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "streams_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "stream_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stream_id"], name: "index_streams_users_on_stream_id"
+    t.index ["user_id"], name: "index_streams_users_on_user_id"
   end
 
   create_table "thoughts", force: :cascade do |t|
