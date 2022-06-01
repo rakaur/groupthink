df4bfe554f4d93f1fdb12a8516848a0ef686ecc5
Eric Will <rakaur@malkier.net>
2022-06-01 01:26:06 -0500

    Fix Stream#content
    Get rid of the `eval`. I'm still not certain whether this should stay as JSON or have its own model. Without an object/model it will be harder to tie the UI elements that manipulate the filters back to something, and I'm concerned the JSON will eventually morph into a giant specification I have to keep track of.

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,19 +1,41 @@
 class Stream < ApplicationRecord
   has_and_belongs_to_many :users
 
+  # content = {
+  #   where: {
+  #     user_id: Integer,
+  #     content: String,
+  #     created_at: DateTime,
+  #     updated_at: DateTime
+  #   },
+  #
+  #   all: true,
+  #   created_ago_start: Integer, # number of seconds ago, ActiveSupport::Duration.to_json
+  #   created_ago_stop: Integer
+  # }
+
   # Returns all matching thoughts
   def thoughts(limit = nil)
     thoughts = Thought.all
 
-    content.first.each do |key, value|
+
+    content.each do |key, value|
       case key
       when "all"
-        thoughts = thoughts.where(created_at: 1.month.ago .. Time.now)
+        thoughts = thoughts.where(created_at: 1.month.ago .. Time.current)
         break
-      when "created_range"
-        thoughts = thoughts.where(created_at: eval(content.first[key]))
+      when "created_ago_start"
+        start = ActiveSupport::Duration.build(content["created_ago_start"].to_i).ago
+
+        if content["created_ago_stop"].present?
+          stop = ActiveSupport::Duration.build(content["created_ago_stop"].to_i).ago
+        else
+          stop = Time.current
+        end
+
+        thoughts = thoughts.where(created_at: start .. stop)
       when "where"
-        thoughts = thoughts.where(content.first[key])
+        thoughts = thoughts.where(content[key])
       else
         # TODO: Unknown key, ignore for now ?
       end
